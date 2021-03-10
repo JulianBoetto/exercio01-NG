ab4e5099ee59f23ec05147d6513acfa42865cd44
julián Boetto <malostratoscba@gmail.com>
2021-03-10 20:43:37 -0300

    Feita a estilizacao

--- src/app/eu/eu.component.ts
+++ src/app/eu/eu.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eu.component.css']
 })
 export class EuComponent implements OnInit {
-  nome: string = "Julian";
+  nome: string = "Julián";
   idade: string = "33 anos";
   gosto: string = "Escutar música, tocar o violão, comer, programar e dormir (muito)";
   pais: string = "Argentina";
